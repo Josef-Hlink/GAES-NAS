6661d196d595e4c11194e6b4bcd8dedab51703c6
Josef <jdhamelink0@gmail.com>
2022-11-08 10:51:08 +0100

    Implement ES for lab-6
    not perfect, but a decent starting point

--- src/labs/lab-6/evolution_strategy_template.py
+++ src/labs/lab-6/evolution_strategy_template.py
@@ -4,6 +4,7 @@ import ioh
 import sys
 import numpy as np
 import time
+from typing import Callable
 
 dimension = 5
 def sphere(x: np.ndarray) -> float:
@@ -32,71 +33,108 @@ sphere.attach_logger(l)
 
 
 # Initialization
-def initialization(mu,dimension,lowerbound = -5.0, upperbound = 5.0):
-    ...
-    return parent,parent_sigma
+def initialization(
+    mu: int,
+    dimension: int,
+    lowerbound = -5.0,
+    upperbound = 5.0
+    ) -> tuple[np.ndarray, np.ndarray]:
+    """ Initializes mu individuals with dimension dimensions, along with their sigma vectors """
+    parents = np.array([
+        np.random.uniform(lowerbound, upperbound, dimension)
+        for _ in range(mu)
+    ])
+    parents_sigma = np.array([
+        np.random.uniform(lowerbound/1000, upperbound/1000, dimension)
+        for _ in range(mu)
+    ])
+    return parents, parents_sigma
+
 
 # Mutation
-def mutation(parent, parent_sigma,tau):
-    ...       
+def mutation(
+    parents: np.ndarray,
+    parents_sigma: np.ndarray,
+    tau: float
+    ) -> tuple[np.ndarray, np.ndarray]:
+    """ Mutation of the parents? (children makes more sense) """
+    for i in range(len(parents)):
+        parents[i] = parents[i] + parents_sigma[i]
+        parents_sigma[i] = parents_sigma[i] * np.exp(tau * np.random.normal(0, 1))
+    return parents, parents_sigma
+
 
 # Recombination
-def recombination(parent,parent_sigma):
-    ...
-    return offspring,sigma
+def recombination(
+    parents: np.ndarray,
+    parent_sigmas: np.ndarray
+    ) -> np.ndarray:
+    """ Recombination of the parents to create one child """
+    offspring = parents[np.random.choice(parents.shape[0])]
+    offspring_sigma = parent_sigmas[np.random.choice(parent_sigmas.shape[0])]
+    return offspring, offspring_sigma
 
-def evolution_strategy(func, budget = None):
+
+def evolution_strategy(
+    func : Callable[[np.ndarray], float],
+    budget: int = None
+    ) -> tuple[float, np.ndarray]:
     
     # Budget of each run: 50,000
     if budget is None:
-        budget = 50000
+        budget = 50_000
     
     f_opt = sys.float_info.max
     x_opt = None
 
     # Parameters setting
-    mu_ = ...
-    lambda_ = ...
-    tau =  ...
+    mu_ = 100
+    lambda_ = 10
+    tau =  1 / np.sqrt(2 * dimension)
 
-    
     # Initialization and Evaluation
-    parent,parent_sigma = initialization(mu_,func.meta_data.n_variables)
-    parent_f = []
-    for i in range(mu_):
-        parent_f.append(func(parent[i]))
-        budget = budget - 1
-        if parent_f[i] < f_opt:
-            f_opt = parent_f[i]
-            x_opt = parent[i].copy()
+    parents, parents_sigma = initialization(mu_, func.meta_data.n_variables)
+    parents_f = np.array([func(parent) for parent in parents])
+    budget = budget - mu_
 
     # Optimization Loop
     while (f_opt > optimum and budget > 0):        
-        offspring = []
-        offspring_sigma = []
-        offspring_f = []
+        offspring = np.zeros(shape=(mu_, dimension))
+        offspring_sigma = np.zeros(shape=(mu_, dimension))
+        offspring_f = np.zeros(shape=(mu_))
 
         # Recombination
-        for i in range(lambda_):
-            o, s = recombination(parent,parent_sigma)
-            offspring.append(o)
-            offspring_sigma.append(s)
+        for i in range(mu_):
+            o, s = recombination(parents, parents_sigma)
+            offspring[i] = o
+            offspring_sigma[i] = s
+            offspring_f[i] = func(o)
+            budget -= 1
 
         # Mutation
-        mutation(offspring,offspring_sigma,tau)
+        offspring, offspring_sigma = mutation(offspring, offspring_sigma, tau)
 
         # Selection
-        parent = []
-        parent_sigma = []
-        parent_f = []
+        ranking = np.argsort(offspring_f)
+        offspring = offspring[ranking]
+        offspring_sigma = offspring_sigma[ranking]
+        offspring_f = offspring_f[ranking]
+
+        parents = offspring[:lambda_]
+        parents_sigma = offspring_sigma[:lambda_]
+        parents_f = offspring_f[:lambda_]
         
-        # Implement a selection operator
-        ...
+        # Update the best solution
+        best_in_pop = np.argmin(parents_f)
+        if parents_f[best_in_pop] < f_opt:
+            f_opt = parents_f[best_in_pop]
+            x_opt = parents[best_in_pop].copy()
 
     # ioh function, to reset the recording status of the function.
     func.reset()
-    print(f_opt,x_opt)
+    print(f_opt, x_opt)
     return f_opt, x_opt
+
 
 def main():
     # We run the algorithm 20 independent times.
