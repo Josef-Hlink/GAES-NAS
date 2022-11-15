dba69494e89ff3fd0713aef3666ab89e7d990024
Josef <jdhamelink0@gmail.com>
2022-11-15 23:06:13 +0100

    Implement GA as class
    probably riddled with bugs, integration will follow soon

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -1,157 +1,330 @@
-from ioh import get_problem
+from datetime import datetime
+
 import numpy as np
-import matplotlib.pyplot as plt
+import ioh
 
+from utils import ProgressBar
 
-def main():
+class GeneticAlgorithm:
 
-    iohp_kwargs = dict(
-        fid = 1,
-        instance = 1,
-        dimension = 100,
-        problem_type = 'PBO'
-    )
+    def __init__(
+        self,
+        problem: ioh.ProblemType,
+        pop_size: int,
+        mu_: int,
+        lambda_: int,
+        budget: int = 5_000,
+        selection: str = 'rw',
+        recombination: str = 'kp',
+        mutation: str = 'u',
+        xp: int = None,
+        mut_rate: float = None,
+        mut_nb: int = None,
+        run_id: any = None,
+        verbose: bool = False
+        ) -> None:
 
-    x, f, log = genetic_algorithm(100, 0.1, 100_000, iohp_kwargs, return_log=True)
-    print(f'Best solution: {x}')
-    print(f'Best fitness: {f}')
-    print(f'Number of generations: {len(log)}')
-    fig = plot_results(log)
-    fig.savefig('../plots/results.png')
+        """ Sets all parameters. """
 
+        if run_id is None:
+            run_id = datetime.now().strftime('%Y%m%d-%H%M%S')
+        kwargs = locals(); kwargs.pop('self')
+        self.validate_parameters(**kwargs)
 
-def genetic_algorithm(
-    population_size: int = 100,
-    mu: float = 0.1,
-    budget: int = 10_000,
-    iohp_kwargs: dict = None,
-    return_log: bool = False
-    ) ->  tuple[np.ndarray, float] | tuple[np.ndarray, float, list[dict]]:
-    
-    """
-    Genetic algorithm
-    ===
-    Parameters
-    ---
-    population_size: int
-        Size of the population.
-    mu: float
-        Survival rate, [0, 1].
-    budget: int
-        Number of function evaluations.
-    iohp_args: dict
-        Arguments for the IOH problem, will be passed directly (defaults to onemax problem).
-    return_log: bool
-        Whether to return the log.
-    
-    Returns
-    ---
-    best_x: np.ndarray
-        Best found solution.
-    best_f: float
-        Fitness value associated with the best solution.
-    log: list[dict] (optional)
-        Log of the algorithm. Each entry is a dictionary with the following keys:
+        self.problem = problem
+        self.pop_size = pop_size
+        self.mu_ = mu_
+        self.lambda_ = lambda_
+        self.budget = budget
+        self.run_id = run_id
+        self.verbose = verbose
+
+        self.n_dimensions = problem.meta_data.n_variables
+
+        if self.pop_size == self.lambda_:
+            self.selection_kind = ','
+        else:  # pop_size is mu + lambda
+            self.selection_kind = '+'
+
+        self.selection = dict(
+            rw = self.selection_roulette_wheel,
+            ts = self.selection_tournament,
+            rk = self.selection_rank,
+            su = self.selection_stochastic_universal
+        )[selection]
+
+        self.recombination = dict(
+            kp = self.recombination_k_point,
+            u = self.recombination_uniform
+        )[recombination]
+
+        if recombination == 'kp':
+            self.xp = xp if xp is not None else 1
+        else:
+            self.xp = None
         
-        * best_so_far (float): Best fitness value found so far.
-        * best_in_pop (float): Best fitness value in the population.
-        * worst_in_pop (float): Worst fitness value in the population.
-        * mean_in_pop (float): Mean fitness value in the population.
-    """
-    
-    if iohp_kwargs is None:
-        iohp_kwargs = dict(
-            fid = 1,
-            instance = 1,
-            dimension = 10,
-            problem_type = 'PBO'
-        )
-    
-    n_survivors = int(population_size*mu)
-    n_dimensions = iohp_kwargs['dimension']
-    p = get_problem(**iohp_kwargs)
-    population = np.random.randint(0, 2, (population_size, n_dimensions))
-    best_x, best_f = np.empty(n_dimensions), -np.inf
-    if return_log:
-        log = []
-    
-    while budget >= population_size:
+        self.mutation = dict(
+            u = self.mutation_uniform,
+            b = self.mutation_bitflip
+        )[mutation]
+
+        if mutation == 'u':
+            self.mut_rate = mut_rate if mut_rate is not None else 1 / self.n_dimensions
+            self.mut_nb = None
+        elif mutation == 'b':
+            self.mut_nb = mut_nb if mut_nb is not None else 1
+            self.mut_rate = None
+
+        self.n_generations = self.budget // self.pop_size
+        self.history = np.zeros((self.n_generations))
+        if self.verbose:
+            self.progress = ProgressBar(self.n_generations)
+
+        self.f_opt = -np.inf  # problem is always a maximization one? TODO check this
+        self.x_opt = None
+
+        return
+
+
+    def optimize(self, return_history: bool = False) -> tuple[np.ndarray, float] | tuple[np.ndarray, float, np.ndarray]:
+        """
+        Runs the optimization algorithm and returns the best candidate solution found and its fitness.
+        If return_history is set to True, it will also return the history
+        of the best fitness value found in each population.
+        """
+
+        self.population = np.random.uniform(2, (self.pop_size, self.n_dimensions))
+
+        for gen in range(self.n_generations):
+            self.population, self.pop_fitness = self.evaluate_population()
+            f_opt_in_pop = self.pop_fitness[0]
+            self.history[gen] = f_opt_in_pop
+
+            if f_opt_in_pop > self.f_opt:
+                self.f_opt = f_opt_in_pop
+                self.x_opt = self.population[0]
+            
+            parents = self.selection(self.population, self.pop_fitness)
+            offspring = self.recombination(parents)
+            offspring = self.mutation(offspring)
+
+            if self.selection_kind == '+':
+                self.population = np.concatenate((parents, offspring), axis=0)
+            else:  # selection_kind is ','
+                self.population = offspring
+            
+            if self.verbose:
+                self.progress(gen)
         
-        # evaluate population
-        fitness = np.array([p(individual) for individual in population])
-        budget -= population_size
+        if self.verbose:
+            print(f'f_opt: {self.f_opt:.2f}')
+            print(f'x_opt: {self.x_opt}')
+
+        if return_history:
+            return self.x_opt, self.f_opt, self.history
+        else:
+            return self.x_opt, self.f_opt
+
+
+    def evaluate_population(self) -> tuple[np.ndarray, float]:
+        """
+        Evaluates the fitness of the population and returns the best fitness value found.
+        Returns the candidate solutions ranked by fitness values, along with the highest fitness value.
+        """
+
+        pop_fitness = np.array([self.problem(x) for x in self.population])
+        ranking = np.argsort(pop_fitness)
+
+        return self.population[ranking], pop_fitness[ranking]
+
+
+    def selection_roulette_wheel(self, population: np.ndarray, pop_fitness: np.ndarray) -> np.ndarray:
+        """ Selects parents using roulette wheel selection. """
+
+        total_fitness = np.sum(pop_fitness)
+        probs = pop_fitness / total_fitness
+        cum_probs = np.cumsum(probs)
+
+        parents = np.zeros((self.mu_, self.n_dimensions))
+
+        for i in range(self.mu_):
+            r = np.random.rand()
+            for j in range(self.pop_size):
+                if r < cum_probs[j]:
+                    parents[i] = population[j]
+                    break
+
+        return parents
+
+    def selection_tournament(self, population: np.ndarray, pop_fitness: np.ndarray) -> np.ndarray:
+        """ Selects parents using tournament selection. """
+
+        parents = np.zeros((self.mu_, self.n_dimensions))
+
+        for i in range(self.mu_):
+            pool = np.random.choice(self.pop_size, 2)  # TODO add parameter to determine pool size
+            parents[i] = population[pool[np.argmax(pop_fitness[pool])]]
+
+        return parents
+    
+    def selection_rank(self, population: np.ndarray, pop_fitness: np.ndarray) -> np.ndarray:
+        """ Selects parents using rank selection. """
+
+        parents = np.zeros((self.mu_, self.n_dimensions))
+        probs = np.arange(self.pop_size, 0, -1) / (self.pop_size * (self.pop_size + 1) / 2)
+
+        for i in range(self.mu_):
+            r = np.random.rand()
+            for j in range(self.pop_size):
+                if r < probs[j]:
+                    parents[i] = population[j]
+                    break
         
-        # necessary logging
-        best_x_in_pop, best_f_in_pop = population[np.argmax(fitness)], np.max(fitness)
-        if best_f_in_pop > best_f:
-            best_x, best_f = best_x_in_pop, best_f_in_pop
+        return parents
 
-        # optional logging
-        if return_log:
-            log.append(
-                dict(
-                    best_so_far = best_f,
-                    best_in_pop = best_f_in_pop,
-                    worst_in_pop = np.min(fitness),
-                    mean_in_pop = np.mean(fitness)
-                )
-            )
+    def selection_stochastic_universal(self, population: np.ndarray, pop_fitness: np.ndarray) -> np.ndarray:
+        """ Selects parents using stochastic universal sampling. """
 
-        # selection
-        parents_ids = np.array([roulette_wheel_selection(fitness) for _ in range(n_survivors)])
-        new_population = []
+        total_fitness = np.sum(pop_fitness)
+        probs = self.pop_fitness / total_fitness
+        cum_probs = np.cumsum(probs)
 
-        # new generation
-        while len(new_population) < population_size:
-            pid_1, pid_2 = np.random.choice(parents_ids, 2, replace=False)
-            parent_1, parent_2 = population[pid_1], population[pid_2]
+        parents = np.zeros((self.mu_, self.n_dimensions))
+        r = np.random.rand() / self.mu_
 
-            # crossover and mutation
-            child = crossover_1p(parent_1, parent_2)
-            child = mutate_uniform(child)
-            new_population.append(child)
+        for i in range(self.mu_):
+            for j in range(self.pop_size):
+                if r < cum_probs[j]:
+                    parents[i] = population[j]
+                    break
+            r += 1 / self.mu_
 
-        population = np.array(new_population)
-
-    if return_log:
-        return best_x, best_f, log
-    else:
-        return best_x, best_f
+        return parents
 
 
-def plot_results(log: list[dict]) -> plt.Figure:
-    """Plot the results of the genetic algorithm."""
-    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-    ax.plot([entry['best_so_far'] for entry in log], label='Best so far')
-    ax.plot([entry['best_in_pop'] for entry in log], label='Best in population')
-    ax.plot([entry['worst_in_pop'] for entry in log], label='Worst in population')
-    ax.plot([entry['mean_in_pop'] for entry in log], label='Mean in population')
-    ax.set_xlabel('Generation')
-    ax.set_ylabel('Fitness')
-    ax.legend()
-    return fig
+    def recombination_k_point(self, parents: np.ndarray) -> np.ndarray:
+        """ Recombines parents using k-point crossover to produce all offspring. """
+
+        offspring = np.zeros((self.lambda_, self.n_dimensions))
+
+        for i in range(0, self.lambda_, 2):
+            pi1, pi2 = np.random.choice(self.mu_, 2, replace=False)
+            points = np.random.choice(range(1, self.n_dimensions), self.xp, replace=False)
+            points.sort()
+
+            prev_point = 0
+            for point in points:
+                offspring[i][prev_point:point] = parents[pi1][prev_point:point]
+                offspring[i+1][prev_point:point] = parents[pi2][prev_point:point]
+                prev_point = point
+                temp = pi1; pi1 = pi2; pi2 = temp  # swap parent ids
+
+            offspring[i][prev_point:] = parents[pi1][prev_point:]
+            offspring[i+1][prev_point:] = parents[pi2][prev_point:]
+
+        return offspring
+
+    def recombination_uniform(self, parents: np.ndarray) -> np.ndarray:
+        """ Recombines parents using uniform crossover to produce all offspring. """
+
+        offspring = np.zeros((self.lambda_, self.n_dimensions))
+
+        for i in range(0, self.lambda_):
+            pi1, pi2 = np.random.choice(self.mu_, 2, replace=False)
+            mask = np.random.choice(2, self.n_dimensions, replace=True)
+            offspring[i] = parents[pi1] * mask + parents[pi2] * (1 - mask)
+            offspring[i+1] = parents[pi2] * mask + parents[pi1] * (1 - mask)
+
+        return offspring
+
+    
+    def mutation_uniform(self, offspring: np.ndarray) -> np.ndarray:
+        """ Mutates the offspring using uniform mutation. """
+
+        for i in range(self.lambda_):
+            for j in range(self.n_dimensions):
+                if np.random.rand() < self.mut_rate:
+                    offspring[i][j] = not offspring[i][j]
+
+        return offspring
+    
+    def mutation_bitflip(self, offspring: np.ndarray) -> np.ndarray:
+        """ Mutates the offspring using bitflip mutation. """
+
+        points = np.random.choice(range(self.n_dimensions), self.mut_nb, replace=False)
+        for i in range(self.lambda_):
+            offspring[i][points] = not offspring[i][points]
+
+        return offspring
 
 
-def roulette_wheel_selection(fitness: np.ndarray) -> int:
-    """Roulette wheel selection."""
-    fitness = fitness - np.min(fitness)
-    fitness = fitness / np.sum(fitness)
-    return np.random.choice(fitness.shape[0], p=fitness)
+    def validate_parameters(
+        self,
+        problem: ioh.ProblemType,
+        pop_size: int,
+        mu_: int,
+        lambda_: int,
+        budget: int,
+        selection: str,
+        recombination: str,
+        mutation: str,
+        xp: int,
+        mut_rate: float,
+        mut_nb: int,
+        run_id: any,
+        verbose: bool
+        ) -> None:
 
-def mutate_uniform(individual: np.ndarray) -> np.ndarray:
-    """Uniform mutation."""
-    for i in range(individual.shape[0]):
-        if np.random.rand() < 1 / individual.shape[0]:
-            individual[i] = not individual[i]
-    return individual
+        """ Validates all parameters passed to the constructor. """
 
-def crossover_1p(parent1: np.ndarray, parent2: np.ndarray) -> np.ndarray:
-    """One point crossover."""
-    cutoff = np.random.choice(parent1.shape[0])
-    child = np.concatenate((parent1[:cutoff], parent2[cutoff:]))
-    return child
+        dims = problem.meta_data.n_variables
 
+        assert isinstance(problem, ioh.ProblemType), "problem must be an instance of <ioh.ProblemType>"
 
-if __name__ == "__main__":
-    np.random.seed(123)
-    main()
+        assert isinstance(pop_size, int), "pop_size must be an integer"
+        assert pop_size in range(0, 500), "pop_size must be between 0 and 500"
+
+        assert isinstance(mu_, int), "mu_ must be an integer"
+        assert mu_ > 0, "mu_ must be greater than 0"
+        assert mu_ < pop_size, "mu_ must be less than population size"
+
+        assert isinstance(lambda_, int), "lambda_ must be an integer"
+        assert lambda_ > 0, "lambda_ must be greater than 0"
+        assert lambda_ <= pop_size, "lambda_ must be less than or equal to population size"
+        assert lambda_ % 2 == 0, "lambda_ must be an even number"
+
+        assert pop_size == lambda_ + mu_ or pop_size == lambda_, "population size must be" + \
+        " either number of parents + number of offspring or just number of offspring"
+
+        assert isinstance(budget, int), "budget must be an integer"
+        assert budget > 0, "budget must be greater than 0"
+        assert budget <= 100_000_000, "budget must be less than or equal to 100 million"
+
+        assert selection in ['rw', 'ts', 'rk', 'su'], "selection must be one of the following: 'rw', 'ts', 'rk', 'su'"
+
+        assert recombination in ['kp', 'u'], "recombination must be one of the following: 'u', 'kp'"
+        if recombination == 'kp':
+            if xp is not None:
+                assert isinstance(xp, int), "for kp crossover, xp must be an integer"
+                assert xp > 0, "for kp crossover, xp must be greater than 0"
+                assert xp < dims, "for kp crossover, xp must be less than problem dimension"
+
+        assert mutation in ['u', 'b'], "mutation must be one of the following: 'u', 'b'"
+        if mutation == 'u':
+            assert mut_nb is None, "for uniform mutation, mut_nb must be None"
+            if mut_rate is not None:
+                assert isinstance(mut_rate, float), "for u, mut_rate must be a float"
+                assert mut_rate > 0, "for u, mut_rate must be greater than 0"
+                assert mut_rate < 1, "for u, mut_rate must be less than 1"
+        if mutation == 'b':
+            assert mut_rate is None, "for normal mutation, mut_rate must be None"
+            if mut_rate is not None:
+                assert isinstance(mut_rate, int), "for n, mut_rate must be an integer"
+                assert mut_rate > 0, "for n, mut_rate must be greater than 0"
+                assert mut_rate < dims, "for n, mut_rate must be less than problem dimension"
+        
+        assert len(str(run_id)) > 0, "run_id must be representable as a string"
+
+        assert isinstance(verbose, bool), "verbose must be a boolean"
+        
+        return
